9e3737494566264ddf9fbc8ee4c7af807fa1d007
jones89-dan <50154382+jones89-dan@users.noreply.github.com>
2024-08-04 22:01:33 -0700

    Updated Product services, to return an observable, and map/unwrap JSON response from Spring Data Rest

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable, map} from "rxjs";
+import {Product} from "../common/product";
+
+
+// Unwrap the JSON from Spring Data REST _embedded entry
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  //Return an observable, map the JSON data from Spring Data Rest to product array
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+      map(response => response._embedded.products)
+    );
+  }
+
+
 }
