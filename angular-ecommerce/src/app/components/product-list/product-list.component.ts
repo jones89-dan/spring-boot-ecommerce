a83774c3dc5291ce204dca7fc12a4aa0c974020d
jones89-dan <50154382+jones89-dan@users.noreply.github.com>
2024-08-04 22:19:29 -0700

    Updated Product List Component, added listProduct method, and added array traversal to product-list.component.html

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../../services/product.service";
+import {Product} from "../../common/product";
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[] = [];
+  // Inject ProductService
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts() {
+    // Method is invoked once you "subscribe"
+    this.productService.getProductList().subscribe(
+      data =>{
+        // Assign results to Product array
+        this.products = data;
+      }
+    )
   }
 
 }
