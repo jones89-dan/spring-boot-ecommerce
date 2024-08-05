6c41c497ffd51310cf2e8328458a872002e2b587
jones89-dan <50154382+jones89-dan@users.noreply.github.com>
2024-08-04 22:32:56 -0700

    Added CORS for product and product category back end, added missing product attribute (name)

--- spring-boot-ecommerce/src/main/java/com/brckbybrck/spring_boot_ecommerce/dao/ProductCategoryRepository.java
+++ spring-boot-ecommerce/src/main/java/com/brckbybrck/spring_boot_ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,10 @@ package com.brckbybrck.spring_boot_ecommerce.dao;
 import com.brckbybrck.spring_boot_ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+// Allow CORS for Angular front end.
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
--- spring-boot-ecommerce/src/main/java/com/brckbybrck/spring_boot_ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/brckbybrck/spring_boot_ecommerce/dao/ProductRepository.java
@@ -3,7 +3,10 @@ package com.brckbybrck.spring_boot_ecommerce.dao;
 import com.brckbybrck.spring_boot_ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+// Allow CORS for Angular front end.
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
